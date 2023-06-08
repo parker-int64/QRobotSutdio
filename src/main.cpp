f82394e4aa438d719936661bf57408c24105f5bb
parker-int64 <huxiaochuan2011@hotmail.com>
2023-06-08 15:40:31 +0800

    Use the info from the generated template

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml/qqmlextensionplugin.h>
 #include <QQuickStyle>
+#include <QTranslator>
 #include "config.h"
 #include <QDebug>
+
+Q_IMPORT_QML_PLUGIN(ExtraUtilsPlugin)
+
+
+// Generic Appication information
+void setApplicationInfo() {
+
+    const QString appName = QString(APP_NAME);
+    const QString appVer  = QString("%1.%2.%3.%4")
+                               .arg(VERSION_MAJOR)
+                               .arg(VERSION_MINOR)
+                               .arg(VERSION_PATCH)
+                               .arg(VERSION_TWEAK);
+    const QString orgName = QString(QObject::tr("ParkerInt64"));  // me of course..
+    const QString orgDomain = QString(APP_HOMEPAGE_URL);
+
+    QGuiApplication::setApplicationVersion(appVer);
+    QGuiApplication::setOrganizationName(orgName);
+    QGuiApplication::setOrganizationDomain(orgDomain);
+    QGuiApplication::setApplicationName(appName);  // Fallback 1, Fallback 2 is the target name in `add_executable`
+
+
+#ifdef _DEBUG
+    qDebug() << "\n\n" << appName.toUtf8().data() << "\n"
+             << QObject::tr("Version: \t").toUtf8().data() << appVer.toUtf8().data() << "\n"
+             << QObject::tr("Author: \t").toUtf8().data() << orgName.toUtf8().data() << "\n"
+             << QObject::tr("Project url: \t").toUtf8().data() << orgDomain.toUtf8().data();
+
+    qDebug() << Qt::endl;
+#endif
+
+}
+
+
 int main(int argc, char *argv[])
 {
+    setApplicationInfo();
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
@@ -28,14 +66,7 @@ int main(int argc, char *argv[])
     engine.loadFromModule("RobotControl", "Main");
 #endif
 
-#ifdef _DEBUG
-    qDebug() << "Program Version: "
-             << QString("%1.%2.%3.%4").arg(VERSION_MAJOR)
-                                   .arg(VERSION_MINOR)
-                                   .arg(VERSION_PATCH)
-                                   .arg(VERSION_TWEAK)
-             << Qt::endl;
-#endif
+
 
     return app.exec();
 }
